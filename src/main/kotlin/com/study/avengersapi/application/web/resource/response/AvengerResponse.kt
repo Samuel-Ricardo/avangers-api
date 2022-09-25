30a666ca16e707b88c17465944937742406856ac
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-09-25 16:08:37 -0300

    :sparkles: create: data class - AvengerResponse.kt

--- src/main/kotlin/com/study/avengersapi/application/web/resource/response/AvengerResponse.kt
+++ src/main/kotlin/com/study/avengersapi/application/web/resource/response/AvengerResponse.kt
@@ -1,5 +1,9 @@
 package com.study.avengersapi.application.web.resource.response
 
 data class AvengerResponse(
-
+    val id: Long?,
+    val nick: String,
+    val person: String,
+    val description: String?,
+    val history: String?,
 )
