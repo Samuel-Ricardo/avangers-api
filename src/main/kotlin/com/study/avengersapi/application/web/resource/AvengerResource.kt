ae2036e8485f18d3d5aa8126672808a71d5e5498
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-09-25 16:17:50 -0300

    :sparkles: create: rest controller - AvengerResource.kt

--- src/main/kotlin/com/study/avengersapi/application/web/resource/AvengerResource.kt
+++ src/main/kotlin/com/study/avengersapi/application/web/resource/AvengerResource.kt
@@ -1,4 +1,15 @@
 package com.study.avengersapi.application.web.resource
 
-class AvengerResource {
-}
+import com.study.avengersapi.domain.avenger.AvengersRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+private const val API_PATH = "/v1/api/avenger"
+
+@RestController
+@RequestMapping(value = [API_PATH])
+class AvengerResource(
+    @Autowired private val repository: AvengersRepository
+)
