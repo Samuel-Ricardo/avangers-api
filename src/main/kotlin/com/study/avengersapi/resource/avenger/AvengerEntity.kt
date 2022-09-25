bfcbe976644f1f4ecce9b62e65136ca7e30637a9
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-09-25 16:59:50 -0300

    :sparkles: create: Entity - AvengerEntity

--- src/main/kotlin/com/study/avengersapi/resource/avenger/AvengerEntity.kt
+++ src/main/kotlin/com/study/avengersapi/resource/avenger/AvengerEntity.kt
@@ -1,3 +1,21 @@
 package com.study.avengersapi.resource.avenger
 
-data class AvengerEntity()
+import net.bytebuddy.dynamic.loading.InjectionClassLoader.Strategy
+import javax.persistence.*
+
+@Entity
+@Table(name = "avenger")
+data class AvengerEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long?,
+
+    @Column(nullable = false)
+    val nick: String,
+    @Column(nullable = false)
+    val person: String,
+
+    val description: String?,
+
+    val history: String?
+)
