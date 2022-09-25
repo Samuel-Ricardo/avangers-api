99d583dd8a8e0137b3c0e92b433971df10342621
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-09-25 16:11:11 -0300

    :sparkles: create: fun - from (AvengerResponse.kt)

--- src/main/kotlin/com/study/avengersapi/application/web/resource/response/AvengerResponse.kt
+++ src/main/kotlin/com/study/avengersapi/application/web/resource/response/AvengerResponse.kt
@@ -1,9 +1,21 @@
 package com.study.avengersapi.application.web.resource.response
 
+import com.study.avengersapi.domain.avenger.Avenger
+
 data class AvengerResponse(
     val id: Long?,
     val nick: String,
     val person: String,
     val description: String?,
     val history: String?,
-)
+) {
+    companion object {
+        fun from(avenger: Avenger) = AvengerResponse(
+            id = avenger.id,
+            nick = avenger.nick,
+            person = avenger.person,
+            description = avenger.description,
+            history = avenger.history
+        )
+    }
+}
