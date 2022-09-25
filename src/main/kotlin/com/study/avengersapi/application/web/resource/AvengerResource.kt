77da8b3b681b32f2d8f7f90ba1d12857ab1deb8f
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-09-25 16:23:05 -0300

    :sparkles: create: get mapping - getAvengers (AvengerResource)

--- src/main/kotlin/com/study/avengersapi/application/web/resource/AvengerResource.kt
+++ src/main/kotlin/com/study/avengersapi/application/web/resource/AvengerResource.kt
@@ -1,7 +1,9 @@
 package com.study.avengersapi.application.web.resource
 
+import com.study.avengersapi.application.web.resource.response.AvengerResponse
 import com.study.avengersapi.domain.avenger.AvengersRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -12,4 +14,12 @@ private const val API_PATH = "/v1/api/avenger"
 @RequestMapping(value = [API_PATH])
 class AvengerResource(
     @Autowired private val repository: AvengersRepository
-)
+) {
+
+    @GetMapping
+    fun getAvengers() = repository.getAvengers()
+        .map { AvengerResponse.from(it) }
+        .let { ResponseEntity.ok().body(it) }
+
+
+}
