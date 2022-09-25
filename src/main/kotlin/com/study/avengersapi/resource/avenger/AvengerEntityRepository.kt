d36f214b5730b63c64981311db95d41b32bda527
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-09-25 17:09:22 -0300

    :sparkles: create: Repository - AvengerEntityRepository

--- src/main/kotlin/com/study/avengersapi/resource/avenger/AvengerEntityRepository.kt
+++ src/main/kotlin/com/study/avengersapi/resource/avenger/AvengerEntityRepository.kt
@@ -1,4 +1,7 @@
 package com.study.avengersapi.resource.avenger
 
-interface AvengerEntityRepository {
-}
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface AvengerEntityRepository: JpaRepository<AvengerEntity, Long>
